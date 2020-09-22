a69c9b8cd6e5533fcc4f23b13376a9a6eb3a804d
intendo <intendo23@gmail.com>
2020-09-21 19:21:32 -0700

    Turn off Python and Shared Libraries

--- .build.sh
+++ .build.sh
@@ -68,7 +68,7 @@ cmake                                                     \
       -DENABLE_OPENMP=ON                                  \
       -DENABLE_PBAM=ON                                    \
       -DENABLE_PBSAM=ON                                   \
-      -DENABLE_PYTHON=ON                                  \
+      -DENABLE_PYTHON=OFF                                 \
       -DENABLE_TESTS=ON                                   \
       -DENABLE_TINKER=OFF                                 \
       -DBUILD_SHARED_LIBS=OFF                             \
