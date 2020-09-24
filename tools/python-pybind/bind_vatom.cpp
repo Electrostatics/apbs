79ef5d95e16f9574e6048a5ea610445713096684
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-09-24 16:41:01 -0700

    cleaning up NOsh bindings

--- tools/python-pybind/bind_vatom.cpp
+++ tools/python-pybind/bind_vatom.cpp
@@ -1,20 +1,11 @@
 #include "bind_vatom.hpp"
-
-/**
- * @brief Class allows for the c-style arrays to be replaced by
- * C++ STL types which more easily interface with python.
- */
-class Vatom_glue : public Vatom
-{
-public:
-  std::array<double, 3> position; /**< Atomic position */
-  std::string resName; /**< Residue name from PDB/PQR file */
-  std::string atomName; /**< Atom name from PDB/PDR file */
-};
+#include <cstdio>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
 
 void bind_vatom(py::module& m)
 {
-  py::class_<Vatom_glue>(m, "Vatom", py::dynamic_attr())
+  py::class_<sVatom>(m, "Vatom")
     .def(py::init<>())
 #ifdef WITH_TINKER
     .def("setInducedDipole"  , &Vatom_setInducedDipole)
@@ -25,24 +16,49 @@ void bind_vatom(py::module& m)
     .def("getQuadrupole"     , &Vatom_getQuadrupole)
     .def("getInducedDipole"  , &Vatom_getInducedDipole)
     .def("getNLInducedDipole", &Vatom_getNLInducedDipole)
-    .def_readwrite("dipole"			     , &Vatom::dipole)  /**< Permanent dipole */
-    .def_readwrite("quadrupole"		   , &Vatom::quadrupole)  /**< Permanent quadrupole */
-    .def_readwrite("inducedDipole"	 , &Vatom::inducedDipole)   /**< Induced dipole */
-    .def_readwrite("nlInducedDipole" , &Vatom::nlInducedDipole)	  /**< Non-local induced dipole */
+    .def_readwrite("dipole"			     , &sVatom::dipole)  /**< Permanent dipole */
+    .def_readwrite("quadrupole"		   , &sVatom::quadrupole)  /**< Permanent quadrupole */
+    .def_readwrite("inducedDipole"	 , &sVatom::inducedDipole)   /**< Induced dipole */
+    .def_readwrite("nlInducedDipole" , &sVatom::nlInducedDipole)	  /**< Non-local induced dipole */
 #endif
-    .def("setResName"        , &Vatom_setResName)
-    .def("setAtomName"       , &Vatom_setAtomName)
-    .def("getResName"        , &Vatom_getResName)
-    .def("getAtomName"       , &Vatom_getAtomName)
-    .def("setPosition"       , &Vatom_setPosition)
     .def("copyTo"            , &Vatom_copyTo)
     .def("copyFrom"          , &Vatom_copyFrom)
-    .def_readwrite("position", &Vatom_glue::position)
-    .def_readwrite("radius"  , &Vatom::radius)
-    .def_readwrite("charge"  , &Vatom::charge)
-    .def_readwrite("partID"  , &Vatom::partID)
-    .def_readwrite("epsilon" , &Vatom::epsilon)
-    .def_readwrite("id"      , &Vatom::id)
-    .def_readwrite("resName" , &Vatom_glue::resName)
-    .def_readwrite("atomName", &Vatom_glue::atomName);
+    .def_readwrite("radius"  , &sVatom::radius)
+    .def_readwrite("charge"  , &sVatom::charge)
+    .def_readwrite("partID"  , &sVatom::partID)
+    .def_readwrite("epsilon" , &sVatom::epsilon)
+    .def_readwrite("id"      , &sVatom::id)
+    .def_property("position",
+        [] (sVatom& self)
+        {
+          return std::vector<double>(self.position, self.position+3);
+        },
+        [] (sVatom& self, py::array_t<double> other)
+        {
+          py::buffer_info buf = other.request();
+          assert(buf.ndim == 1 && "Vatom::position is 1D!");
+          assert(other.size() == 3 && "Vatom::position has length 3!");
+          auto* ptr = static_cast<double*>(buf.ptr);
+          for(int i=0;i<3;i++) self.position[i] = ptr[i];
+        })
+    .def_property("atomName",
+        &Vatom_getAtomName,
+        [] (sVatom& self, std::string other)
+        {
+          for(int i=0;i<VMAX_RECLEN;i++)
+          {
+            if(other.c_str()[i]=='\0') break;
+            self.atomName[i] = other.c_str()[i];
+          }
+        })
+    .def_property("resName",
+        &Vatom_getResName,
+        [] (sVatom& self, std::string other)
+        {
+          for(int i=0;i<VMAX_RECLEN;i++)
+          {
+            if(other.c_str()[i]=='\0') break;
+            self.resName[i] = other.c_str()[i];
+          }
+        });
 }
