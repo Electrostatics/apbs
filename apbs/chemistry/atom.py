8c029dea900dd05e830b5d20ad4dfe7d5b4e81f0
intendo <intendo23@gmail.com>
2020-10-02 15:46:00 -0700

    Updated Atom docs, force types

--- apbs/chemistry/atom.py
+++ apbs/chemistry/atom.py
@@ -20,39 +20,27 @@ class Atom:
         """
         Arguments:
 
-        :param id: A unique identifier for this Atom
-        :type id: int
-        :param field_name: Specifies the type of PQR entry and should either
+        :param int id: A unique identifier for this Atom
+        :param str field_name: Specifies the type of PQR entry and should either
                            be ATOM or HETATM in order to be parsed by APBS.
-        :type field_name: string
-        :param atom_number: The atom index.
-        :type atom_number: int
-        :param atom_name: The atom name.
-        :type atom_name: string
-        :param residue_name: The residue name.
-        :type residue_name: string
-        :param chain_id: An optional value which provides the chain ID of
+        :param int atom_number: The atom index.
+        :param str atom_name: The atom name.
+        :param str residue_name: The residue name.
+        :param str chain_id: An optional value which provides the chain ID of
                          the atom. NOTE: that chain ID support is a new
                          feature of APBS 0.5.0 and later versions.
-        :type chain_id: string
-        :param residue_number: The residue index.
-        :type residue_number: int
-        :param ins_code: An optional value which provides the PDB insertion code.
-        :type ins_code: string
-        :param x: The X atomic coordinate in angstroms
-        :type x: float
-        :param y: The Y atomic coordinate in angstroms
-        :type y: float
-        :param z: The Z atomic coordinate in angstroms
-        :type z: float
-        :param charge: The atomic charge (in electrons).
-        :type charge: float
-        :param radius: The atomic radius (in angstroms).
-        :type radius: float
+        :param int residue_number: The residue index.
+        :param str ins_code: An optional value which provides the PDB insertion code.
+        :param float x: The X atomic coordinate in angstroms
+        :param float y: The Y atomic coordinate in angstroms
+        :param float z: The Z atomic coordinate in angstroms
+        :param float charge: The atomic charge (in electrons).
+        :param float radius: The atomic radius (in angstroms).
 
         :Example:
 
           atom = Atom(
+                    id=42,
                     field_name=ATOM,
                     atom_number=39,
                     atom_name=O3PB,
@@ -64,36 +52,36 @@ class Atom:
                     y=-6.763,
                     z=26.980,
                     charge=-0.900,
-                    radius=1.700,
-                    id=1,
+                    radius=1.700
                 )
         """
+
+        if (len(args) > 0):
+            self.position = Coordinate(args[0], args[1], args[2])
+
         self.field_name: str = kwargs.get("field_name", None)
-        self.atom_number: int = kwargs.get("atom_number", 0)
+        self.atom_number: int = int(kwargs.get("atom_number", 0))
         self.atom_name: str = kwargs.get("atom_name", None)
         self.residue_name: str = kwargs.get("residue_name", None)
         self.chain_id: str = kwargs.get("chain_id", None)
-        self.residue_number: int = kwargs.get("residue_number", 0)
+        self.residue_number: int = int(kwargs.get("residue_number", 0))
         self.ins_code: str = kwargs.get("ins_code", None)
-        self.position = Coordinate()
         if "x" in kwargs and "y" in kwargs and "z" in kwargs:
             self.position = Coordinate(
-                kwargs.get("x"), kwargs.get("y"), kwargs.get("z")
+                float(kwargs.get("x")), float(kwargs.get("y")), float(kwargs.get("z"))
             )
-        self.charge: float = kwargs.get("charge", 0.0)
-        self.radius: float = kwargs.get("radius", 0.0)
-        self.epsilon: float = kwargs.get("epsilon", 0.0)
-        self.id: int = kwargs.get("id", 0)
-        # TODO: The ID must get set to be unique! We can implement this
-        #       once we fix all the tests.
-        # if ("id" not in kwargs):
-        #     raise ValueError("The Atom id must be set to non-zero value")
+        self.charge: float = float(kwargs.get("charge", 0.0))
+        self.radius: float = float(kwargs.get("radius", 0.0))
+        self.epsilon: float = float(kwargs.get("epsilon", 0.0))
+        self.id: int = int(kwargs.get("id", 0))
+        if ("id" not in kwargs):
+            raise ValueError("The Atom id must be set to non-zero value")
 
     def __str__(self):
-        return f"Atom< name< {self.name} >, {self.position}, radius< {self.radius} >, charge< {self.charge} > >"
+        return f"Atom< name< {self.field_name} >, {self.position}, radius< {self.radius} >, charge< {self.charge} > >"
 
     def __repr__(self):
-        return f"Atom< name< {self.name} >, {self.position}, radius< {self.radius} >, charge< {self.charge} > >"
+        return f"Atom< name< {self.field_name} >, {self.position}, radius< {self.radius} >, charge< {self.charge} > >"
 
     def euclidian_dist2(
         self, other: Union["Atom", Coordinate, np.ndarray]
