1b9be0bb10ee986ccb30f3c6ba3d70a7c93f9e00
Asher Mancinelli <ashermancinelli@gmail.com>
2020-10-22 23:57:13 +0000

    adding more comprehensive driver

--- apbs/__main__.py
+++ apbs/__main__.py
@@ -1,7 +1,14 @@
 
+import os
 import apbs.bin
 import sys
 import subprocess
 
 apbspath = os.path.join(apbs.bin.get_path(), 'apbs')
-subprocess.run([str(apbspath), *sys.argv[1:])
+if len(sys.argv) < 2:
+    raise RuntimeError('You must provide a valid binary. Please see `apbs.bin.get_path()` for valid binaries.')
+bin = sys.argv[1]
+if len(sys.argv) > 2:
+    subprocess.run([str(os.path.join(apbs.bin.get_path(), sys.argv[1])), *sys.argv[2:]])
+else:
+    subprocess.run([str(os.path.join(apbs.bin.get_path(), sys.argv[1]])
