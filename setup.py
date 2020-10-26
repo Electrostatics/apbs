f113c1da0648aa335db22d155843cc235147a765
intendo <intendo23@gmail.com>
2020-10-26 09:54:03 -0700

    Apply black formatting

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ from tools.setup_helpers.extra_setuptools_commands import *
 from setuptools import setup, find_packages
 
 pp = pprint.PrettyPrinter(indent=2)
-proj_name = 'apbs'
+proj_name = "apbs"
 
-if not os.path.exists(os.path.join('.', __file__)):
-    print('-- Must run setup.py in project root directory!')
+if not os.path.exists(os.path.join(".", __file__)):
+    print("-- Must run setup.py in project root directory!")
     sys.exit()
 
-if os.path.exists('MANIFEST'):
-    os.remove('MANIFEST')
+if os.path.exists("MANIFEST"):
+    os.remove("MANIFEST")
 
 proj_root = os.path.dirname(os.path.abspath(__file__))
 old_path = os.getcwd()
@@ -27,7 +27,7 @@ sys.path.insert(0, proj_root)
 
 # This class must be in setup.py to find package root reliably
 class CleanBuild(Command):
-    '''Clean build directory'''
+    """Clean build directory"""
 
     user_options = []
 
@@ -40,35 +40,36 @@ class CleanBuild(Command):
     def run(self):
         build_dir = get_build_dir()
         rmtree(build_dir)
-        for subdir in ('lib', 'bin'):
-            for f in os.listdir(os.path.join(proj_root, 'apbs', subdir)):
-                if not f.endswith('.py'):
-                    os.remove(os.path.join(proj_root, 'apbs', subdir, f))
+        for subdir in ("lib", "bin"):
+            for f in os.listdir(os.path.join(proj_root, "apbs", subdir)):
+                if not f.endswith(".py"):
+                    os.remove(os.path.join(proj_root, "apbs", subdir, f))
+
 
 extra_cmake_args = dict(
-    CMAKE_BUILD_TYPE='Release',
-    ENABLE_BEM='ON',
-    ENABLE_GEOFLOW='ON',
-    ENABLE_FETK='ON',
-    ENABLE_OPENMP='ON',
-    ENABLE_PBAM='ON',
-    ENABLE_PBSAM='ON',
-    ENABLE_PYTHON='ON',
-    ENABLE_TESTS='ON',
-    ENABLE_TINKER='OFF',
-    )
+    CMAKE_BUILD_TYPE="Release",
+    ENABLE_BEM="ON",
+    ENABLE_GEOFLOW="ON",
+    ENABLE_FETK="ON",
+    ENABLE_OPENMP="ON",
+    ENABLE_PBAM="ON",
+    ENABLE_PBSAM="ON",
+    ENABLE_PYTHON="ON",
+    ENABLE_TESTS="ON",
+    ENABLE_TINKER="OFF",
+)
 
-if os.environ.get('Python_ROOT_DIR', None):
-    extra_cmake_args['Python_ROOT_DIR'] = os.environ['Python_ROOT_DIR']
+if os.environ.get("Python_ROOT_DIR", None):
+    extra_cmake_args["Python_ROOT_DIR"] = os.environ["Python_ROOT_DIR"]
 
 # Skip installation step if building a whl
-if 'bdist' in sys.argv or 'bdist_wheel' in sys.argv:
-    os.environ['DO_INSTALL'] = '0'
+if "bdist" in sys.argv or "bdist_wheel" in sys.argv:
+    os.environ["DO_INSTALL"] = "0"
 
 raw_package_data = {
-    'apbs.bindings': glob.glob(os.path.join('apbs', 'bindings', '*')),
-    'apbs.lib': glob.glob(os.path.join('apbs', 'lib', '*')),
-    'apbs.bin': glob.glob(os.path.join('apbs', 'bin', '*')),
+    "apbs.bindings": glob.glob(os.path.join("apbs", "bindings", "*")),
+    "apbs.lib": glob.glob(os.path.join("apbs", "lib", "*")),
+    "apbs.bin": glob.glob(os.path.join("apbs", "bin", "*")),
 }
 
 # Filter out python and pyc files from package data
@@ -76,36 +77,37 @@ package_data = dict()
 for k, v in raw_package_data.items():
     package_data[k] = []
     for f in v:
-        if not f.endswith('.py') and not '__pycache__' in f:
+        if not f.endswith(".py") and not "__pycache__" in f:
             package_data[k].append(os.path.basename(f))
 
-print('Package data:')
+print("Package data:")
 pp.pprint(package_data)
 
 setup(
     name=proj_name,
-    version='3.0.0',
-    author='',
-    author_email='asher.mancinelli@pnnl.gov',
-    description='Adaptive Poisson-Boltzmann Solver',
-    long_description=open('LICENSE.md', 'r').read(),
-    ext_modules=[CMakeExtension(
-        f'{proj_root}.apbs.bindings.pybind',
-        **extra_cmake_args)],
+    version="3.0.0",
+    author="",
+    author_email="asher.mancinelli@pnnl.gov",
+    description="Adaptive Poisson-Boltzmann Solver",
+    long_description=open("LICENSE.md", "r").read(),
+    ext_modules=[
+        CMakeExtension(f"{proj_root}.apbs.bindings.pybind", **extra_cmake_args)
+    ],
     distclass=BinaryDistribution,
     cmdclass={
-        'build_ext': CMakeBuild,
-        'install': CMakeInstall,
-        'develop': CMakeInstall,
-        'clean': CleanBuild,
-        'sdist': SDistChecked,
-        },
+        "build_ext": CMakeBuild,
+        "install": CMakeInstall,
+        "develop": CMakeInstall,
+        "clean": CleanBuild,
+        "sdist": SDistChecked,
+    },
     zip_safe=False,
-    packages=find_packages(include=['apbs', 'apbs.*']),
+    packages=find_packages(include=["apbs", "apbs.*"]),
     include_package_data=True,
     package_data=package_data,
     classifiers=[
-        'Programming Language :: C++',
-        'Programming Language :: Python :: 3',
-        ] + [f'Programming Language :: Python :: 3.{i}' for i in range(6, 9)],
-    )
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3",
+    ]
+    + [f"Programming Language :: Python :: 3.{i}" for i in range(6, 9)],
+)
