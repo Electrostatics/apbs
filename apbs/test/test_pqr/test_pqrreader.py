02b53cf9b7382703088a771fd8d80f6ca01fec65
intendo <intendo23@gmail.com>
2021-01-21 18:37:21 -0800

    Make tests more strict, add more cases, check for failures, and add --runslow option to parse all pqr files in examples directory

--- apbs/test/test_pqr/test_pqrreader.py
+++ apbs/test/test_pqr/test_pqrreader.py
@@ -1,31 +1,65 @@
 import pathlib
+import pytest
+from pyparsing import ParseException
 from apbs.chemistry.atom_list import AtomList
 from apbs.pqr import PQRReader
 
 
+def search_dir(opt_path):
+    return (
+        pathlib.Path(__file__).parent.parent.parent.parent.absolute()
+        / "examples"
+        / opt_path
+    )
+
+
 class TestPQRReader:
-    def test_ctor(self):
+    def test_basic(self):
+        """This test values in the records in the sample data"""
         sut = PQRReader()
         sample = r"""
 ATOM   5226  HD1 TYR   337     -24.642  -2.718  30.187  0.115 1.358
-ATOM      7  CD   LYS D   1      44.946 37.289  9.712    -0.0608  1.9080
-REMARK This is just a comment hiding in the data
+                  
 HETATM     39 O3PB ADP     1     -16.362  -6.763  26.980 -0.900 1.700
+ATOM      7  CD   LYS D   1      44.946 37.289  9.712    -0.0608  1.9080
+REMARK This is just a   comment hid ing in the data
 ATOM     39 O3PB ADP     1  D   -16.362  -6.763  26.980 -0.900 1.700
+"""
+        atomlist: AtomList = sut.loads(sample)
+        assert len(atomlist) == 4
+        assert atomlist[1].field_name in "HETATM"
+        for idx in range(1):
+            assert abs(atomlist[idx].x - -24.642) < 0.001
+            assert int(atomlist[idx].x) == -24
+
+    @pytest.mark.xfail(
+        raises=ParseException,
+        reason="This should fail because of typo in data",
+    )
+    def test_bad(self):
+        """This test will fail because there is a typo in the sample data"""
+        sut = PQRReader()
+        sample = r"""
+ATOM   5226  HD1 TYR   337     -24.642  -2.718  30.187  0.115 1.358
 REMARK The next line is incorrect on purpose (e.g. ATAM instead of ATOM)
 ATAM     39 O3PB ADP     1  D   -16.362  -6.763  26.980 -0.900 1.700
 """
         atomlist: AtomList = sut.loads(sample)
-        for idx in range(1):
-            assert abs(atomlist.atoms[idx].x - -24.642) < 0.001
-            assert int(atomlist.atoms[idx].x) == -24
-        print(atomlist.atoms)
 
     def test_load(self):
+        """Test to load all the data from an example file"""
         sut = PQRReader()
-        pqr_imput = (
-            pathlib.Path(__file__).parent.parent.parent.parent.absolute()
-            / "examples/actin-dimer/mol1.pqr"
-        )
+        pqr_imput = search_dir("actin-dimer/mol1.pqr")
         atomlist: AtomList = sut.load(pqr_imput)
-        assert len(atomlist.atoms) == 5861
+        assert len(atomlist) == 5877
+
+    @pytest.mark.slow
+    def test_load_all(self):
+        """Test to load all the data from all the example files"""
+        sut = PQRReader()
+        # NOTE: There are 77 files under the examples directory
+        matches = pathlib.Path(search_dir("")).glob("**/*.pqr")
+        for match in matches:
+            print(f"FILE: {match}")
+            atomlist: AtomList = sut.load(match)
+            assert len(atomlist) > 0
