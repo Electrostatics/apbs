ea1fc7e6bcdb6650fc0b75d764dfbae9e4c17e0d
Noah Oblath <noah.oblath@pnnl.gov>
2021-10-07 13:48:37 -0700

    Configure test builds to look at Geoflow, PBAM, and PBSAM builds

--- .build.sh
+++ .build.sh
@@ -112,8 +112,8 @@ cmake -S .. -B $BUILD_DIR \
       -DCMAKE_BUILD_TYPE=$RELEASE_TYPE                    \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR                 \
       -DENABLE_PYGBE=OFF                                   \
-      -DENABLE_BEM=ON                                     \
-      -DENABLE_GEOFLOW=OFF                                \
+      -DENABLE_BEM=OFF                                     \
+      -DENABLE_GEOFLOW=ON                                \
       -DENABLE_FETK=ON                                   \
       -DENABLE_OPENMP=ON                                  \
       -DENABLE_PBAM=ON                                    \
