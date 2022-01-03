2083997630284aa17fd71dc6f434583fb4754af1
Noah Oblath <noah.oblath@pnnl.gov>
2022-01-03 13:06:36 -0800

    Cleanup

--- examples/run_selected_examples.sh
+++ examples/run_selected_examples.sh
@@ -4,21 +4,16 @@ APBS_EXE="apbs"
 BASE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 export PATH="$BASE_PATH/../../../bin:$PATH"
 cd $BASE_PATH
-EXCLUDE="pygbe"
 
 echo $BASE_PATH
 which apbs
-
-DIRS=`find . -maxdepth 1 -type d -name "[a-zA-Z]*" | awk -F'/' '{print $NF}' | grep -v $EXCLUDE`
-
-echo $DIRS
 
 run_example() {
   run_subdir=$1
   in_file=$2
 
   curr_dir=`pwd`
-  echo "current dir: $curr_dir"
+  echo "\ncurrent dir: $curr_dir"
   cd $run_subdir
   if [[ $? -ne 0 ]]; then
     echo "Invalid subdirectory: $run_subdir}"
@@ -71,24 +66,3 @@ exit $total_status
 #run_example "actin-dimer/apbs-smol-parallel.in" (seg fault, fails, 139; not compiled with MPI)
 #run_example "pbam/toy_electrostatic.in" (fails, 13)
 #run_example "pbsam-gly/msms/gly_electrostatic.in" (fails, 13)
-
-
-#for dir in $DIRS
-#do
-#  cd $BASE_PATH
-#  cd $dir
-#  for infile in `ls -1 *.in`
-#  do
-#    echo -n "$APBS_EXE $dir/$infile "
-#    SECONDS=0
-#    #/dev/null 2>&1
-#    $APBS_EXE $infile > OUTPUT.txt 2>&1
-#    status=$?
-#    duration=$SECONDS
-#    echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
-#    if [[ "$status" -ne "0" ]]; then
-#      echo "***** $infile, $status: failed"
-#    fi
-#  done
-#done
-
