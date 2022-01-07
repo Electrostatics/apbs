7a421f3b758ae709554366de637ea0018be96cde
Noah Oblath <noah.oblath@pnnl.gov>
2022-01-07 13:50:59 -0800

    Don't clean the install and build dirs.  Let cmake take care of what it needs to take care of.  And rm -rf'ing a directory passed to a script without any validation is scary.

--- .build.sh
+++ .build.sh
@@ -91,10 +91,10 @@ echo "==================================== PWD FOR TOP DIR =====================
 pwd
  
 echo "==================================== CLEAN =============================================== "
-rm -rf $BUILD_DIR                                         || exit 1
-rm -rf $INSTALL_DIR                                       || exit 1
-mkdir -p $BUILD_DIR                                       || exit 1
-mkdir -p $INSTALL_DIR                                     || exit 1
+#rm -rf $BUILD_DIR                                         || exit 1
+#rm -rf $INSTALL_DIR                                       || exit 1
+#mkdir -p $BUILD_DIR                                       || exit 1
+#mkdir -p $INSTALL_DIR                                     || exit 1
 echo "==================================== SETUP ENV =============================================== "
 export LD_LIBRARY_PATH=$HOME/apbs/lib:${LD_LIBRARY_PATH}
 export PATH=$HOME/apbs/bin:${PATH}
