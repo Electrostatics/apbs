f333b9b22c53805fd5c8f64dad84e30febb94dcf
Noah Oblath <noah.oblath@pnnl.gov>
2022-04-29 16:28:42 -0700

    Read the version from the VERSION file for apbs/_version.py

--- apbs/_version.py
+++ apbs/_version.py
@@ -5,4 +5,8 @@ Store the version here so:
 * we can import it in setup.py for the same reason
 * we can import it into your module
 """
-__version__ = "3.4.1"
+
+file = open('../VERSION', 'r')
+lines = file.readlines()
+verLine = [x for x in lines if x and not x.startswith('#')]
+__version__ = verLine[0].strip().replace('_', '.')
